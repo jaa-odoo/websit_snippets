8145c8e694b56a7c484d9c791c14ee4b4fc8f2f4
Jainik Patel <jp@kanakinfosystems.com>
2020-05-29 18:29:08 +0530

    [ADD] website snippest Module.

---
+++ website_snippets/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Dummy Theme',
+    'description': 'Dummy Theme',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'Kanak InfoSystem LLP',
+    'depends': ['website'],
+    'data': [
+        'views/snippets.xml',
+        'views/options.xml',
+    ],
+    'application': True,
+}
